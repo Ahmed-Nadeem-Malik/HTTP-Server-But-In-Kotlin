ffc5d057eb8c82215b035d56448f0987d7c8133a
Ahmed-Nadeem-Malik <ahmedmalik200625@gmail.com>
2026-01-05 04:34:20 +0000

    echo client server

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,37 +3,17 @@ package org.example
 import java.net.Socket
 
 fun main() {
-    var count = 50
-    val times = ArrayList<Long>()
-    while (count > 0) {
-        val startTime = System.nanoTime()
-        val socket = Socket("learnxinyminutes.com", 80)
-        val reader = socket.getInputStream().bufferedReader()
-        val writer = socket.getOutputStream().bufferedWriter()
+    val socket = Socket("localhost", 8000)
 
-
-        writer.write("GET /kotlin/ HTTP/1.1\r\n")
-        writer.write("Host: learnxinyminutes.com\r\n")
-        writer.write("Connection: close\r\n")
-        writer.write("\r\n")
-        writer.flush()
-        val finishTime = System.nanoTime()
-        --count
-        socket.close()
-        val finalTime =finishTime - startTime
-        times.add(finalTime)
-    }
-
-    times.forEach { println(it) }
-
-    /*
-
+    val reader = socket.getInputStream().bufferedReader()
     val writer = socket.getOutputStream().bufferedWriter()
-    writer.write("hello world")
-    writer.newLine() // submits it to the buffer
-    writer.flush() // sends buffer to the server
+
+    writer.write("Hello World!")
+    writer.newLine()
+    writer.flush()
+
+    val returnMessage = reader.readLine()
+    println(returnMessage)
 
     socket.close()
-
-     */
 }
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -3,14 +3,17 @@ package org.example
 import java.net.ServerSocket
 
 fun main() {
-
     val server = ServerSocket(8000)
-
     val client = server.accept()
 
     val reader = client.getInputStream().bufferedReader()
+    val writer = client.getOutputStream().bufferedWriter()
+
     val message = reader.readLine()
-    print("Client message: $message")
+
+    writer.write(message)
+    writer.newLine()
+    writer.flush()
 
     server.close()
     client.close()
